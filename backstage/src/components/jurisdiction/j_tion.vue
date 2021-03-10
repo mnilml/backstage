a5055b2dd72d9bac5b509a62e04f1ec75df796f0
‘lvy’ <2051777197@qq.com>
2021-03-10 20:14:53 +0800

    lyday2

---
+++ backstage/src/components/jurisdiction/j_tion.vue
@@ -0,0 +1,18 @@
+// 权限管理
+<template>
+  <div>权限管理</div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {},
+};
+</script>
+
+<style scoped lang="less">
+</style>
